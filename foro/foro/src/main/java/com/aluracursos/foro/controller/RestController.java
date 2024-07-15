33f1ef9470cafda130a8b315c861c34ac9a4d7ba
CamilaMajey <90106667+CamilaMajey@users.noreply.github.com>
2024-07-15 08:17:07 -0500

    Delete foro/foro/src directory

--- foro/foro/src/main/java/com/aluracursos/foro/controller/RestController.java
+++
@@ -1,4 +0,0 @@
-package com.aluracursos.foro.controller;
-
-public @interface RestController {
-}
--- foro/foro/src/main/java/com/aluracursos/foro/repository/TopicoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aluracursos.foro.repository;
-
-import com.aluracursos.foro.model.Topico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TopicoRepository extends JpaRepository<Topico, Long> {
-}
